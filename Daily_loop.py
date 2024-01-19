14070b4cf1b78f90c5d130895ff582218a40bfff
LLL1997 <32997505+LLL1997@users.noreply.github.com>
2024-01-19 18:59:57 +0800

    修bug

--- Daily_loop.py
+++ Daily_loop.py
@@ -1,5 +1,6 @@
 import subprocess
 import schedule,time
+json_config_file_path=''
 def daily():
     print("每日")
     # 指定要运行的 Python 脚本
@@ -10,11 +11,11 @@ def daily():
     except subprocess.CalledProcessError as e:
         print(f"命令执行错误，退出码：{e.returncode}")
         print(f"命令输出：{e.output}")
-def Touch_Head(config_file_path):
+def Touch_Head(json_config_file_path):
     try:
         print("摸头")
         baah_path = "main.py "
-        command = f"python {baah_path} {config_file_path}"
+        command = f"python {baah_path} {json_config_file_path}"
         subprocess.run(command)
     except subprocess.CalledProcessError as e:
         print(f"命令执行错误，退出码：{e.returncode}")
@@ -30,10 +31,9 @@ def daily_loop():
     schedule.every().day.at("23:00").do(Touch_Head1)
     schedule.every().day.at("02:30").do(Touch_Head1) # 刷新前摸一次头
 if __name__ == '__main__':
+    Touch_Head1=Touch_Head(json_config_file_path)
     json_config_file_path = 'config_only_touch_head.json'# 专门用来摸头，参数填写只摸头的json配置文件名
-    
-    Touch_Head1=Touch_Head(json_config_file_path) 
-    #daily() # 日常清体力用，使用默认的config
+    daily() # 日常清体力用，使用默认的config
     daily_loop()
 
     while True:
